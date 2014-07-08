e3ba7a6d1cb46fd340a849d22603f630c969a02d
Brian Cox <briancox730@gmail.com>
2014-07-08 17:33:45 -0400

    formatting for edit form

--- vendor/assets/javascripts/custom.js
+++ vendor/assets/javascripts/custom.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
   $('.pr-row').on('click', function() {
     $('.added-row').remove();
-    var row = $(this);
-    var value = row.find('.value').html();
-    var movement = row.find('td:first').html();
+    var value = $(this).find('.value').html();
+    var movement = $(this).find('td:first').html();
     var values = [.5, .55, .6, .65, .7, .75, .8, .85, .9, .95];
     for (i=0; i<values.length; i++) {
-      row.after('<tr class=\"added-row\"><td><strong>'+movement+'</strong> - '+values[i]+'%</td><td><strong>'+(value * values[i]).toFixed(2)+'</strong></td></tr>')
+      $(this).after('<tr class=\"added-row\"><td><strong>'+movement+'</strong> - '+values[i]+'%</td><td><strong>'+(value * values[i]).toFixed(2)+'</strong></td></tr>')
     };
   });
 });
