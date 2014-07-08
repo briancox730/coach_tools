5a633de454b2ed941fbb522461721554cddd178a
Brian Cox <briancox730@gmail.com>
2014-07-08 10:00:14 -0400

    Modified prgrams view

--- app/controllers/personal_records_controller.rb
+++ app/controllers/personal_records_controller.rb
@@ -1,12 +1,10 @@
 class PersonalRecordsController < ApplicationController
   def index
     @pr = PersonalRecord.find(params[:user_id])
-    binding.pry
   end
 
   def edit
     @pr = PersonalRecord.find(params[:id])
-    binding.pry
 
   end
 
--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,6 +1,6 @@
 class ProgramsController < ApplicationController
   def show
-
+    @program = Program.find(params[:id])
   end
 
 end
