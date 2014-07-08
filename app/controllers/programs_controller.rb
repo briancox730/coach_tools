ca44cf35b8491b99e94297b2dc3ba3b6de097fba
Brian Cox <briancox730@gmail.com>
2014-07-08 15:17:24 -0400

    Program display and routing

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,5 +1,7 @@
 class ProgramsController < ApplicationController
   def show
     @program = Program.find(params[:id])
+    @wods = Wod.where(program_id: params[:id]).order(created_at: :desc)
+    @users = User.where(program_id: params[:id])
   end
 end
