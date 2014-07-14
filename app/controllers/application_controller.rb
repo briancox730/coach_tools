22412f30e3944dd80557ee4532e5f0edf3bc8627
Brian Cox <briancox730@gmail.com>
2014-07-14 19:27:58 -0400

    trying to set devise session new as root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    users_path(current_user)
+  end
+
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'users#show'
-
   devise_for :users
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   resources :wods, only: [:show, :new, :create] do
     resources :workouts, only: [:create]
