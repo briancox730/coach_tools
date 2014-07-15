9f5eef98e08f7b212eb0780701c7392c5288a36f
Brian Cox <briancox730@gmail.com>
2014-07-14 20:15:55 -0400

    more view changes for mobile and routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    users_path(current_user)
+    user_path(current_user)
   end
 
   protected
