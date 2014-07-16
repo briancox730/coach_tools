630eb96b4e8bb9d7779d5278bd8d6045ec45108f
Brian Cox <briancox730@gmail.com>
2014-07-16 15:04:00 -0400

    seems to be working

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -91,7 +91,7 @@ class Statistic < ActiveRecord::Base
     avg_dead = @program.average(:dead_lift).to_i
     avg_total = avg_bench_press + avg_squat + avg_dead
     data = {
-    labels: ["bench_press", "Squat", "Deadlift", "Total"],
+    labels: ["Bench", "Squat", "Deadlift", "Total"],
     datasets: [
         {
             label: "User",
@@ -136,7 +136,7 @@ class Statistic < ActiveRecord::Base
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [user_snatch, user_cnj, user_total]
+            data: [user_snatch.round(2), user_cnj.round(2), user_total.round(2)]
         },
         {
             label: "Group Average",
@@ -146,7 +146,7 @@ class Statistic < ActiveRecord::Base
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [avg_snatch, avg_cnj, avg_total]
+            data: [avg_snatch.round(2), avg_cnj.round(2), avg_total.round(2)]
         }
     ]}
   end
@@ -174,7 +174,7 @@ class Statistic < ActiveRecord::Base
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [user_press, user_squat, user_dead, user_total]
+            data: [user_press.round(2), user_squat.round(2), user_dead.round(2), user_total.round(2)]
         },
         {
             label: "Group Average",
@@ -184,7 +184,7 @@ class Statistic < ActiveRecord::Base
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [avg_press, avg_squat, avg_dead, avg_total]
+            data: [avg_press.round(2), avg_squat.round(2), avg_dead.round(2), avg_total.round(2)]
         }
     ]}
   end
@@ -204,7 +204,7 @@ class Statistic < ActiveRecord::Base
     avg_dead = @program.average(:dead_lift).to_f / avg_body_weight
     avg_total = (@program.average(:bench_press) + @program.average(:back_squat).to_f + @program.average(:dead_lift).to_f) / avg_body_weight
     data = {
-    labels: ["bench_press", "Squat", "Deadlift", "Total"],
+    labels: ["Bench", "Squat", "Deadlift", "Total"],
     datasets: [
         {
             label: "User",
@@ -214,7 +214,7 @@ class Statistic < ActiveRecord::Base
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [user_bench_press, user_squat, user_dead, user_total]
+            data: [user_bench_press.round(2), user_squat.round(2), user_dead.round(2), user_total.round(2)]
         },
         {
             label: "Group Average",
@@ -224,7 +224,7 @@ class Statistic < ActiveRecord::Base
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [avg_bench_press, avg_squat, avg_dead, avg_total]
+            data: [avg_bench_press.round(2), avg_squat.round(2), avg_dead.round(2), avg_total.round(2)]
         }
     ]}
   end
