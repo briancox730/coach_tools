1e419b77daf0a4912558e68fb3c3158345977c20
Brian Cox <briancox730@gmail.com>
2014-07-17 14:35:46 -0400

    add admins

---
+++ db/migrate/20140717182550_add_admin_namespacing.rb
@@ -0,0 +1,5 @@
+class AddAdminNamespacing < ActiveRecord::Migration
+  def change
+    add_column :users, :admin, :boolean, default: false;
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716015728) do
+ActiveRecord::Schema.define(version: 20140717182550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,21 +59,22 @@ ActiveRecord::Schema.define(version: 20140716015728) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "program_id",                          null: false
-    t.string   "first_name",                          null: false
-    t.string   "last_name",                           null: false
+    t.integer  "program_id",                             null: false
+    t.string   "first_name",                             null: false
+    t.string   "last_name",                              null: false
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
