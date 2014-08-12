4b2dd221334f83c996015a376afad4eb3968bcff
Brian Cox <briancox730@gmail.com>
2014-08-12 14:49:11 -0400

    added grouping for statistics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,28 +6,29 @@ class UsersController < ApplicationController
 
   def show
     @time = Time.now.strftime("%y%m%d")
-    @complete = Statistic.where(user_id: params[:id]).order(created_at: :desc).includes(:workout).limit(10)
     @user = User.find(params[:id])
+    @complete = @user.statistics.order(created_at: :desc).includes(:workout).limit(10)
+    @complete2 = @user.statistics.includes(:workout).group_by { |statistic| statistic.workout.wod_id }
     if current_user.id == @user.id
-      if !@user.program.wods.find_by(name: @time).nil?
+      if @user.program.wods.find_by(name: @time).nil?
+        @to_complete = []
+      else
         @available_workouts = @user.program.wods.find_by(name: @time).workouts
         @to_complete = Statistic.build_to_complete(@available_workouts, @complete).reverse
-      else
-        @to_complete = []
       end
     end
     @statistic = Statistic.new
-    if !@user.personal_record.snatch.nil? && !@user.personal_record.clean_and_jerk.nil?
-      @oly_data = Statistic.build_oly_data(params[:id])
-      @oly_relative_data = Statistic.build_oly_relative_data(params[:id])
+    if @user.personal_record.snatch.present? && @user.personal_record.clean_and_jerk.present?
+      @oly_data = Statistic.build_oly_data(@user.id)
+      @oly_relative_data = Statistic.build_oly_relative_data(@user.id)
     end
-    if !@user.personal_record.press.nil? && !@user.personal_record.back_squat.nil? && !@user.personal_record.dead_lift.nil?
-      @cf_data = Statistic.build_cf_data(params[:id])
-      @cf_relative_data = Statistic.build_cf_relative_data(params[:id])
+    if @user.personal_record.press.present? && @user.personal_record.back_squat.present? && @user.personal_record.dead_lift.present?
+      @cf_data = Statistic.build_cf_data(@user.id)
+      @cf_relative_data = Statistic.build_cf_relative_data(@user.id)
     end
-    if !@user.personal_record.bench_press.nil? && !@user.personal_record.back_squat.nil? && !@user.personal_record.dead_lift.nil?
-      @power_data = Statistic.build_power_data(params[:id])
-      @power_relative_data = Statistic.build_power_relative_data(params[:id])
+    if @user.personal_record.bench_press.present? && @user.personal_record.back_squat.present? && @user.personal_record.dead_lift.present?
+      @power_data = Statistic.build_power_data(@user.id)
+      @power_relative_data = Statistic.build_power_relative_data(@user.id)
     end
   end
 end
