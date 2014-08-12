42ec131c94f29d8f8a4692b31138f499bb589ec1
Brian Cox <briancox730@gmail.com>
2014-08-12 15:03:12 -0400

    Formatting return data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def show
     @time = Time.now.strftime("%y%m%d")
     @user = User.find(params[:id])
-    @complete = @user.statistics.order(created_at: :desc).includes(:workout).limit(10)
+    @complete = @user.statistics.order(created_at: :desc).includes(:workout)
     @complete2 = @user.statistics.includes(:workout).group_by { |statistic| statistic.workout.wod_id }
     if current_user.id == @user.id
       if @user.program.wods.find_by(name: @time).nil?
