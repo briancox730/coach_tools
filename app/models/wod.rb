c12523a00ed9534c995594a839af13e1c4b0e6ec
Brian Cox <briancox730@gmail.com>
2014-12-15 14:02:14 -0500

    associated workouts

--- app/models/wod.rb
+++ app/models/wod.rb
@@ -10,7 +10,7 @@ class Wod < ActiveRecord::Base
   def self.associated_workouts(wod)
     results = []
 
-    wod.program.workouts.limit(25).each do |w|
+    wod.program.workouts.limit(25).sort_by(&:created_at).reverse.each do |w|
       name = w.wod.name.to_s + w.name.to_s + ' ' + Date.strptime(w.wod.name, "%y%m%d").strftime('%A')
 
       if !w.movement.nil?
