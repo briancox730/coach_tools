24ee7464076aa95782e2d3cfbc50e6b051c32068
Brian Cox <briancox730@gmail.com>
2015-01-15 10:38:35 -0500

    more wods shown

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -6,6 +6,6 @@ class ProgramsController < ApplicationController
 
   def show
     @program = Program.includes(:users).find(params[:id])
-    @wods = Wod.where(program_id: params[:id]).order(name: :desc).limit(10)
+    @wods = Wod.where(program_id: params[:id]).order(name: :desc).limit(30)
   end
 end
