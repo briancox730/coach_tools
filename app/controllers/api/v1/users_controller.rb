a754ceff79bf100a7578cb0e4115912ad67f5059
Brian Cox <briancox730@gmail.com>
2015-03-23 15:19:19 -0400

    branch introduced bug

--- app/controllers/api/v1/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::UsersController < Api::V1::ApiController
-  before_action :authenticate_user!
-
-  def show
-    @to_complete = current_user.workouts_to_complete
-    @statistics = current_user.statistics
-    render json: @statistics, status: :ok
-  end
-end
